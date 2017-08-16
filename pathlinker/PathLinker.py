b0dd67750ff25b2126cd9d44411c052f63cff240
Mitchell Wagner <mitchw94@vt.edu>
2017-08-16 13:53:37 -0400

    Add unit test for file input, fix newline skipping bug

--- pathlinker/PathLinker.py
+++ pathlinker/PathLinker.py
@@ -12,6 +12,7 @@ Virginia Tech, Blacksburg, VA
 Manuscript under review.
 
 This code is authored by:
+Mitch Wagner: mitchw94@vt.edu
 Nicholas Sharp: nsharp3@vt.edu
 Anna Ritz: annaritz@vt.edu
 Christopher L. Poirel: chris.poirel@gmail.com
@@ -28,8 +29,29 @@ import networkx as nx
 
 # local modules
 import ksp_Astar as ksp
-from PageRank import pagerank
-from PageRank import writePageRankWeights
+from pagerank.PageRank import pagerank, writePageRankWeights
+
+# The general idea here is as follows:
+# PathLinker by default can contain large, redundant paths that consist of
+# largely smaller paths with mostly the same edges. The idea here is to
+# FORCE PathLinker to use N unique edges. We do this by adding duplictes of
+# the graph in layers, and for each edge (u,v) in the original graph,
+# add add (u, v2) to the second layer, (u2, v3) to the third layer, etc.
+# We place the target at the top level.
+
+# Note: it might be important to allow the setting of a "minimum path length"
+# parameter as well. This would mean creating shortcuts from the minimum
+# path level to the sink level.
+
+# Note: some graphs might not be able to reach maximum height
+def modifyGraphForKSP_addLayers(net):
+    None
+
+def addWithinLayerEdges(net, layers):
+    None
+
+def addBetweenLayerEdges(net, layers):
+    None
 
 # Modifies the structure of the graph by removing all edges entering
 # sources. These edges will never contribute to a
@@ -197,6 +219,49 @@ def printEdgeFluxes(f, graph):
     outf.close()
     return
 
+def readNetworkFile(network_file, pagerank=False):
+    
+    net = nx.DiGraph()
+
+    # Read the network file
+    # infile = open(network_file, 'r')
+    for line in network_file:
+        items = [x.strip() for x in line.rstrip().split('\t')]
+
+        # Skip empty lines or those beginning with '#' comments
+        if line=='\n':
+            continue
+        if line[0]=='#':
+            continue
+
+        id1 = items[0]
+        id2 = items[1]
+
+        # Ignore self-edges
+        if id1==id2:
+            continue
+
+        # Possibly use an edge weight
+        eWeight = 1
+
+        if (not pagerank):
+            if(len(items) > 2):
+                eWeight = float(items[2])
+
+            else:
+                print("""\nERROR: All edges must have a weight, unless 
+                         --PageRank is used. Edge (%s --> %s) does not have a 
+                         weight entry.""" % (id1, id2))
+                exit(-1)
+                
+        # Assign the weight. Note in the PageRank case, "weight" is
+        # interpreted as running PageRank and edgeflux on a weighted
+        # graph. 
+        net.add_edge(id1, id2, ksp_weight=eWeight, weight=eWeight)
+
+    return net
+    
+
 def main(args):
     usage = '''
 PathLinker.py [options] NETWORK NODE_TYPES
@@ -282,41 +347,10 @@ REQUIRED arguments:
     if(opts.PageRank and opts.no_log_transform):
         sys.exit('\nERROR: Options --PageRank and --no-log-transform should not be used together. PageRank weights are probabilities, and must be log-transformed to have an additive interpretation.')
 
+    net = None
     ## Read the network from file
-    net = nx.DiGraph()
-
-    # Read the network file
-    print('\nReading the network from %s' %(NETWORK_FILE))
-    infile = open(NETWORK_FILE, 'r')
-    for line in infile:
-        items = [x.strip() for x in line.rstrip().split('\t')]
-
-        # Skip empty lines or those beginning with '#' comments
-        if line=='':
-            continue
-        if line[0]=='#':
-            continue
-
-        id1 = items[0]
-        id2 = items[1]
-
-        # Ignore self-edges
-        if id1==id2:
-            continue
-
-        # Possibly use an edge weight
-        eWeight = 1
-        if(len(items) > 2):
-            eWeight = float(items[2])
-        elif(not opts.PageRank):
-            print("\nERROR: All edges must have a weight, unless --PageRank is used. Edge (%s --> %s) does not have a weight entry."%(id1, id2))
-            exit(-1)
-
-        # Assign the weight. Note in the PageRank case, "weight" is
-        # interpreted as running PageRank and edgeflux on a weighted
-        # graph. 
-        net.add_edge(id1, id2, ksp_weight=eWeight, weight=eWeight)
-
+    with open(NETWORK_FILE, 'r') as network_file:
+        net = readNetworkFile(network_file, opts.PageRank)
 
     # Print info about the network
     print(nx.info(net))
---
+++ tests/test_PathLinker.py
@@ -0,0 +1,59 @@
+import unittest
+import networkx as nx
+from io import StringIO
+
+import pathlinker.PathLinker as pl
+
+class TestReadNetworkFile(unittest.TestCase):
+
+    def test_empty_file(self):
+        contents = StringIO(u'')
+        net = pl.readNetworkFile(contents, True) 
+        self.assertEqual(type(net), nx.DiGraph)
+        self.assertEqual(len(net.nodes()), 0)
+        self.assertEqual(len(net.edges()), 0)
+
+    def test_small_network_with_weights(self):
+        contents = StringIO(u'1\t2\t.5\n1\t3\t.5')
+        net = pl.readNetworkFile(contents, True) 
+        self.assertEqual(type(net), nx.DiGraph)
+        self.assertEqual(len(net.nodes()), 3)
+        self.assertEqual(len(net.edges()), 2)
+
+    def test_small_network_empty_lines_are_ignored(self):
+        contents = StringIO(u'\n\n\n\n\n1\t2\t.5\n\n1\t3\t.5\n\n\n')
+        net = pl.readNetworkFile(contents, True) 
+        self.assertEqual(type(net), nx.DiGraph)
+        self.assertEqual(len(net.nodes()), 3)
+        self.assertEqual(len(net.edges()), 2)
+
+    def test_small_network_commented_lines_are_ignored(self):
+        contents = StringIO(u'#comment\n1\t2\t.5\n#comment\n1\t3\t.5')
+        net = pl.readNetworkFile(contents, True) 
+        self.assertEqual(type(net), nx.DiGraph)
+        self.assertEqual(len(net.nodes()), 3)
+        self.assertEqual(len(net.edges()), 2)
+
+    def test_self_edges_are_ignored(self):
+        contents = StringIO(u'1\t1\t.5\n1\t3\t.5')
+        net = pl.readNetworkFile(contents, True) 
+        self.assertEqual(type(net), nx.DiGraph)
+        self.assertEqual(len(net.nodes()), 2)
+        self.assertEqual(len(net.edges()), 1)
+
+
+    def test_no_weights_pagerank_false_exit(self):
+        contents = StringIO(u'1\t1\n1\t3')
+        with self.assertRaises(SystemExit):
+            net = pl.readNetworkFile(contents, False) 
+        
+    def test_no_weights_pagerank_true(self):
+        contents = StringIO(u'1\t1\n1\t3')
+        net = pl.readNetworkFile(contents, True) 
+        self.assertEqual(type(net), nx.DiGraph)
+        self.assertEqual(len(net.nodes()), 2)
+        self.assertEqual(len(net.edges()), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
